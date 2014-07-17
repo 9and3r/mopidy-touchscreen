d78ab738157175884998de070cb1e9d37602183d
Ander <9and3r@gmail.com>
2014-07-17 15:58:13 +0200

    Some changes

--- mopidy_touchscreen/main_screen.py
+++ mopidy_touchscreen/main_screen.py
@@ -2,6 +2,7 @@ import pygame
 import logging
 from mopidy.audio import PlaybackState
 from .dynamic_background import DynamicBackground
+import musicbrainzngs as musicb
 
 
 logger = logging.getLogger(__name__)
@@ -14,19 +15,26 @@ class MainScreen():
 		self.size=size
 		self.manager=manager
 		self.background=DynamicBackground()
+		self.track = None
 
 	def update(self,core):
 		screen = pygame.Surface(self.size)
 		self.background.drawBackground(screen)
 		text = pygame.font.SysFont("arial",20)
-		if(self.manager.track!=None):
-			text_surface=text.render(self.manager.track.track.name,False,(255,255,255))
+		if(self.track!=None):
+			text_surface=text.render(self.track.album.musicbrainz_id,False,(255,255,255))
 			screen.blit(text_surface,(0,0))
-			images=self.manager.track.track.album.images
-			if len(images)>0:
-				image = pygame.image.load(next(iter(images)))
-				screen.blit(image,(0,0))
+			#logger.error(self.track.album.musicbrainz_id)
 		return screen
+
+	def track_started(self,track):
+		self.track=track
+		self.downloadImage()
+
+	def downloadImage(self):
+		logger.error("hemen nago")
+		logger.error(musicb.search_releases(artist=self.track.artist.name, limit=1))
+		
 
 
 	
