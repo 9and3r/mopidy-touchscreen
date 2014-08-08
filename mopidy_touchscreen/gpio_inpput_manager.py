17414dddf33270ca1f9a39d32831e187b7935736
9and3r <9and3r@gmail.com>
2014-08-08 11:44:41 +0200

    GPIO more code

--- mopidy_touchscreen/gpio_inpput_manager.py
+++ mopidy_touchscreen/gpio_inpput_manager.py
@@ -7,6 +7,8 @@ logger = logging.getLogger(__name__)
 class GPIOManager():
 
     def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
         GPIO.add_event_detect(23, GPIO.RISING, callback=self.printFunction, bouncetime=300)
 
 
