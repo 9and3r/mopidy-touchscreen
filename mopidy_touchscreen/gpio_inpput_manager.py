f39706f002509d5afcfe54e094e0eb2d3f76847b
9and3r <9and3r@gmail.com>
2014-08-08 13:51:03 +0200

    GPIO

--- mopidy_touchscreen/gpio_inpput_manager.py
+++ mopidy_touchscreen/gpio_inpput_manager.py
@@ -10,17 +10,17 @@ class GPIOManager():
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        GPIO.add_event_detect(24, GPIO.BOTH, callback=right(), bouncetime=30)
+        GPIO.add_event_detect(24, GPIO.BOTH, callback=right, bouncetime=30)
 
 
 def right(channel):
     dict = {}
     if GPIO.input(channel) == 1:
-        dict['type'] = pygame.KEYUP
+        type = pygame.KEYUP
     else:
-        dict['type'] = pygame.KEYDOWN
+        type = pygame.KEYDOWN
     dict['key'] = pygame.K_RIGHT
-    event = pygame.event.Event(dict)
+    event = pygame.event.Event(type, dict)
     pygame.event.post(event)
 
 
