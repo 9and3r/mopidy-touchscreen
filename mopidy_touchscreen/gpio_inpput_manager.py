0a86db02c72a7cefa0795898ef87f981b5862860
9and3r <9and3r@gmail.com>
2014-08-08 13:45:37 +0200

    GPIO

--- mopidy_touchscreen/gpio_inpput_manager.py
+++ mopidy_touchscreen/gpio_inpput_manager.py
@@ -10,7 +10,7 @@ class GPIOManager():
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        GPIO.add_event_detect(24, GPIO.BOTH, callback=self.rigth, bouncetime=30)
+        GPIO.add_event_detect(24, GPIO.BOTH, callback=self.right(), bouncetime=30)
 
     def right(self, channel):
         dict = {}
