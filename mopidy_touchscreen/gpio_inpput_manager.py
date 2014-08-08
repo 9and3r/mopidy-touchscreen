f15ce5aaa673bb40e36c4dae585ac8874add2ec8
9and3r <9and3r@gmail.com>
2014-08-08 11:54:39 +0200

    GPIO

--- mopidy_touchscreen/gpio_inpput_manager.py
+++ mopidy_touchscreen/gpio_inpput_manager.py
@@ -9,8 +9,8 @@ class GPIOManager():
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-        GPIO.add_event_detect(23, GPIO.RISING, callback=self.printFunction, bouncetime=300)
+        GPIO.add_event_detect(23, GPIO.RISING, callback=printFunction, bouncetime=300)
 
 
-    def printFunction(self):
-        logger.error("Sakatu da")
+def printFunction():
+    logger.error("Sakatu da")
