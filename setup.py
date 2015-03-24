8fb4f2fe2353be25d5c7c510a9feb25f7d2b1c3f
Ander <9and3r@gmail.com>
2015-03-24 11:24:33 +0100

    Change dependency to Mopidy 1.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'setuptools',
-        'Mopidy >= 0.18',
+        'Mopidy >= 1.0',
         'Pykka >= 1.1',
         'pygame'
     ],
