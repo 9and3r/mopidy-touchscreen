10ce0177c491b2fba167e67df8c225d5ca27e45f
Ander <9and3r@gmail.com>
2015-04-18 01:39:40 +0200

    Fixed on_stop

--- mopidy_touchscreen/touch_screen.py
+++ mopidy_touchscreen/touch_screen.py
@@ -77,16 +77,15 @@ class TouchScreen(pykka.ThreadingActor, core.CoreListener):
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.running = False
+                    utils.process.exit_process()
                 elif event.type == pygame.KEYUP and event.key == pygame.K_q:
-                    self.running = False
+                    utils.process.exit_process()
                 elif event.type == pygame.VIDEORESIZE:
                     self.get_display_surface(event.size)
                     self.screen_manager.resize(event)
                 else:
                     self.screen_manager.event(event)
         pygame.quit()
-        utils.process.exit_process()
 
     def on_start(self):
         try:
